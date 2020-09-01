71d3c294b7b8417d4ac131b105efa431fc54b43f
Lubovin Vyacheslav <C:\Users\vlubovin\AppData\Roaming\The Bat!>
2020-09-01 14:32:39 +0300

    First

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: NotFoundComponent }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
